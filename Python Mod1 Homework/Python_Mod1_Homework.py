fa39d673dcd03f22d41c4057a4404ec587af479f
franc <franc@BLUE>
2024-07-12 19:57:40 -0700

    Add project files.

---
+++ Python Mod1 Homework/Python_Mod1_Homework.py
@@ -0,0 +1,19 @@
+#------challenge exercise #1 -- PERSONAL INFORMATION PROGRAM
+
+#INPUT SCREEN
+name = input('Enter your First and Last name: ')
+addy = input('Enter your Address: ')
+city = input("Enter your City: ")
+state = input('Enter your State: ')
+zipCode = str(input('Enter your ZipCode: ')) 
+phone = str(input('Enter your Mobile Number: '))
+major = input('Enter your College Major: ')
+
+#OUTPUT 
+print(name + "\n" + addy + "\n" + city + ", " + state + ', ' + zipCode + "\n" +
+       phone + "\n" + major)
+
+
+
+
+
---
+++ Python Mod1 Homework/class_1.py
@@ -0,0 +1,25 @@
+## Author: Francisco Bumanglag
+## Project: Personal Information Form
+## Assignment: Module 1 Project 1
+## Course: Python Santa Ana College
+## Class: CMPR114 Jason Sim
+## Date: June 18, 2023
+
+
+#INPUT SCREEN
+name = input('Enter your First and Last name: ')
+addy = input('Enter your Address: ')
+city = input("Enter your City: ")
+state = input('Enter your State: ')
+zipCode = str(input('Enter your ZipCode: ')) 
+phone = str(input('Enter your Mobile Number: '))
+major = input('Enter your College Major: ')
+
+#OUTPUT 
+print(name + "\n" + addy + "\n" + city + ", " + state + ', ' + zipCode + "\n" +
+       phone + "\n" + major)
+
+
+
+
+
